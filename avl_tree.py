d7e8f35f23a84a93826d0f9dfffabb0ebeddb121
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2026-03-01 16:26:31 -0500

    forgot repr method for avl node class

--- avl_tree.py
+++ avl_tree.py
@@ -1,12 +1,16 @@
 #!python3
 
 class AVLNode:
-    """a single node in the avl tree storing a key-value pair."""
+    """a single node in the avl tree storing a key-value pair"""
 
     def __init__(self, key, value=None):
-        """initialize node with given key and optional value."""
+        """initialize node with given key and optional value"""
         self.key = key
         self.value = value
         self.left = None
         self.right = None
         self.height = 1
+
+    def __repr__(self):
+        """return a string representation of this node"""
+        return f'AVLNode({self.key!r})'
