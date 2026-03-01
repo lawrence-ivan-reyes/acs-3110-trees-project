b1f1aae7188e9e22d44f1831cae248f4241f5462
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2026-03-01 16:25:00 -0500

    add avl node class

---
+++ avl_tree.py
@@ -0,0 +1,12 @@
+#!python3
+
+class AVLNode:
+    """a single node in the avl tree storing a key-value pair."""
+
+    def __init__(self, key, value=None):
+        """initialize node with given key and optional value."""
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
