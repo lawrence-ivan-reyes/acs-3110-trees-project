afd2ea4499ec8b909ab4922dc0b91e90a4c38c6b
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2026-03-02 13:22:57 -0500

    add browse & filter methods

--- movie_db.py
+++ movie_db.py
@@ -31,6 +31,29 @@ class MovieDatabase:
         """search for a movie by exact title"""
         return self.by_title.get(title)
 
+    def all_by_title(self):
+        """return all movies sorted alphabetically by title"""
+        return self.by_title.values()
+
+    def all_by_rating(self):
+        """return all movies sorted by rating (ascending)"""
+        return self.by_rating.values()
+
+    def filter_by_genre(self, genre):
+        """return all movies matching a genre, sorted by title"""
+        results = []
+        for title, movie in self.by_title.items():
+            if movie['genre'].lower() == genre.lower():
+                results.append(movie)
+        return results
+
+    def top_rated(self, n=10):
+        """return top n movies by rating"""
+        all_movies = list(self.by_rating.items())
+        # reverse since by_rating is ascending!!
+        all_movies.reverse()
+        return [movie for _, movie in all_movies[:n]]
+
     @property
     def size(self):
         return len(self.by_title)
