e428a8e212cc609717da9afc3788fc430876908b
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2026-03-02 13:10:47 -0500

    add tree map dunder methods + main demo

--- tree_map.py
+++ tree_map.py
@@ -40,3 +40,56 @@ class OrderedTreeMap:
     def items(self):
         """return all (key, value) pairs in sorted key order"""
         return self._tree.items_in_order()
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, key):
+        result = self._tree.search(key)
+        if result is None:
+            raise KeyError(key)
+        return result
+
+    def __delitem__(self, key):
+        self.remove(key)
+
+    def __contains__(self, key):
+        return self._tree.contains(key)
+
+    def __len__(self):
+        return len(self._tree)
+
+    def __iter__(self):
+        """iterate over keys in sorted order"""
+        for key, value in self._tree.items_in_order():
+            yield key
+
+    def __repr__(self):
+        """return string representation of this map"""
+        items = ', '.join(f'{k!r}: {v!r}' for k, v in self._tree.items_in_order())
+        return f'OrderedTreeMap({{{items}}})'
+
+
+def main():
+    movie_map = OrderedTreeMap()
+
+    movie_map['Parasite'] = 8.5
+    movie_map['Whiplash'] = 8.5
+    movie_map['Prisoners'] = 8.2
+    movie_map['The Prestige'] = 8.5
+    movie_map['Oddity'] = 6.7
+
+    print(f'Map size: {len(movie_map)}')
+    print(f'Whiplash rating: {movie_map["Whiplash"]}')
+    print(f'Contains "Parasite": {"Parasite" in movie_map}')
+
+    print('\nAll movies (sorted by title):')
+    for key in movie_map:
+        print(f'  {key}: {movie_map[key]}')
+
+    del movie_map['Oddity']
+    print(f'\nAfter deleting "Oddity": {len(movie_map)} movies')
+
+
+if __name__ == '__main__':
+    main()
